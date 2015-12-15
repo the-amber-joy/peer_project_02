fe411bddd30de98ca0bb540ddab3003c58ea879f
Amber Joy <amber.joy@gmail.com>
2015-12-15 16:52:53 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,33 @@
+var names = ["Kenzie", "Amber", "Altamir", "Mark", "Joe", "Scott", "Anthony", "Brooks", "Natalie", "Matthew", "Charlie", "Chris", "Jeremy", "Eric", "Nathan", "Robby", "Sam", "Zach", "Liz", "Paul"];
+
+var ticker = -1;
+
+$(document).ready(function(){
+	$('#all-names').append('<button class="button">Click here</button>');
+	$('#all-names').on('click', '.button', revealName);
+	$('.person').on('click', fadeName);
+
+
+});
+
+function revealName(){
+	var $el = $('#all-names').children().last();
+
+	ticker += 1;
+	$el.parent().append('<div class="person">' + names[ticker] + '</div>');			
+	
+	if (names[ticker] == undefined){
+		$('.person').remove();
+		ticker = -1;
+		};
+	
+	console.log(ticker);
+	
+	}
+
+function fadeName(){
+	$('.person').fadeOut//fade to 5%
+
+}
+
+
